b258e602b57f692c82dfbd188778709a25f455fa
Andres Araya <araya.andres@gmail.com>
2017-08-20 09:19:17 -0600

    Get path using DFS.

--- ch2/maze.cc
+++ ch2/maze.cc
@@ -5,13 +5,14 @@
 #include <iostream>
 #include <set>
 #include <vector>
+#include <tuple>
 
 enum class Cell: char {
     Empty = ' ',
     Blocked = '#',
     Start = 'S',
     Goal = 'G',
-    Path = 'P',
+    Path = '.',
 };
 
 std::ostream& operator<<(std::ostream& os, Cell c) {
@@ -60,6 +61,11 @@ void set_start_location(Maze& m, const Location& s) {
     m[s.row][s.col] = Cell::Start;
 }
 
+void set_goal_location(Maze& m, const Location& g) {
+    assert(is_within_maze(m, g));
+    m[g.row][g.col] = Cell::Goal;
+}
+
 void print_maze(const Maze& m) {
     for (const auto& r : m) {
         for (const auto& c : r) {
@@ -84,28 +90,63 @@ Successors successors_for_maze(const Maze& m, const Location& l) {
     return s;
 }
 
-void dfs(const Maze& m, const Location& start, const Location& goal) {
-    auto frontier = successors_for_maze(m, start);
-    std::set<Location> explored{start};
+struct Node {
+    Location l;
+    const Node * prev;
+};
+
+bool operator<(const Node& lhs, const Node& rhs) {
+    return lhs.l < rhs.l;
+}
+
+bool operator==(const Node& lhs, const Node& rhs) {
+    return lhs.l == rhs.l;
+}
+
+using Path = std::vector<Location>;
+
+Path build_path(const Node* n) {
+    Path p;
+    while (n) {
+        p.push_back(n->l);
+        n = n->prev;
+    }
+    return p;
+}
+
+Path dfs(const Maze& m, const Location& start, const Location& goal) {
+    std::set<Node> explored{{start, nullptr}};
+    std::vector<Node> frontier;
+    for (const auto& l : successors_for_maze(m, start)) frontier.push_back({l, nullptr});
     while (!frontier.empty()) {
-        auto l = frontier.back();
-        if (l == goal) {
-            return;
+        auto current_node = frontier.back();
+        if (current_node.l == goal) {
+            return build_path(current_node.prev);
         }
         frontier.pop_back();
-        explored.insert(l);
-        for (const auto& s : successors_for_maze(m, l)) {
-            if (explored.find(s) == explored.end()) frontier.push_back(s);
+        auto [it, _] = explored.insert(current_node);
+        for (const auto& s : successors_for_maze(m, current_node.l)) {
+            Node n{s, &*it};
+            if (explored.find(n) == explored.end()) frontier.push_back(n);
         }
     }
+    return {};
+}
+
+Maze draw_path(Maze m, Path p) {
+    for (const auto& l : p) {
+        m[l.row][l.col] = Cell::Path;
+    }
+    return m;
 }
 
 int main() {
     srand(time(nullptr));
     const Location start_location{5,5};
+    const Location goal_location{0,0};
     auto m = generate_maze(10, 10, 0.2);
     set_start_location(m, start_location);
-    dfs(m,{5,5},{0,0});
-    print_maze(m);
-    return 0;
+    set_goal_location(m, goal_location);
+    auto p = dfs(m, start_location, goal_location);
+    print_maze(draw_path(m, p));
 }
