1624087a0757cc1be35f8cccb5f2e6e69396a199
Andres Araya <andres-francisco.araya@hpe.com>
2017-08-22 17:54:31 -0700

    Missionaries and cannibals.

--- ch2/missionaries.cc
+++ ch2/missionaries.cc
@@ -1,3 +1,4 @@
+#include "range/v3/all.hpp"
 #include <iostream>
 #include <vector>
 
@@ -10,13 +11,29 @@ std::ostream& operator<<(std::ostream& os, Side s) {
 }
 
 struct MCState {
-    const int missionaries;
-    const int cannibals;
-    const Side side;
-    int west_bank_missionaries() const { return missionaries; }
-    int west_bank_cannibals() const { return cannibals; }
-    int east_bank_missionaries() const { return MAX_NUM - missionaries; }
-    int east_bank_cannibals() const { return MAX_NUM - cannibals; }
+    const int missionaries_;
+    const int cannibals_;
+    const Side side_;
+    int missionaries() const {
+        return side_ == Side::WEST
+            ? west_bank_missionaries()
+            : east_bank_missionaries();
+    }
+    int cannibals() const {
+        return side_ == Side::WEST
+            ? west_bank_cannibals()
+            : east_bank_cannibals();
+    }
+    Side side() const { return side_; }
+    int west_bank_missionaries() const { return missionaries_; }
+    int west_bank_cannibals() const { return cannibals_; }
+    int east_bank_missionaries() const { return MAX_NUM - missionaries_; }
+    int east_bank_cannibals() const { return MAX_NUM - cannibals_; }
+    Side other_side() const {
+        return side_ == Side::WEST
+            ? Side::EAST
+            : Side::WEST;
+    }
     bool is_legal() const {
         return !(west_bank_missionaries() < west_bank_cannibals() && west_bank_missionaries() > 0)
             && !(east_bank_missionaries() < east_bank_cannibals() && east_bank_missionaries() > 0);
@@ -24,16 +41,18 @@ struct MCState {
 };
 
 bool operator==(const MCState& lhs, const MCState& rhs) {
-    return lhs.missionaries == rhs.missionaries
-        && lhs.cannibals == rhs.cannibals
-        && lhs.side == rhs.side;
+    return lhs.missionaries_ == rhs.missionaries_
+        && lhs.cannibals_ == rhs.cannibals_
+        && lhs.side_ == rhs.side_;
 }
 
 std::ostream& operator<<(std::ostream& os, const MCState& s) {
     return os
-        << "On the west bank there are " << s.missionaries << " missionaries and " << s.cannibals << " cannibals.\n"
-        << "On the east bank there are " << (MAX_NUM - s.missionaries) << " missionaries and " << (MAX_NUM - s.cannibals) << " cannibals.\n"
-        << "The boat is on the " << s.side << " bank\n";
+        << "On the west bank there are " << s.west_bank_missionaries()
+        << " missionaries and " << s.west_bank_cannibals() << " cannibals.\n"
+        << "On the east bank there are " << s.east_bank_missionaries()
+        << " missionaries and " << s.east_bank_cannibals() << " cannibals.\n"
+        << "The boat is on the " << s.side() << " bank\n";
 }
 
 bool goal_test(const MCState& s) {
@@ -41,22 +60,42 @@ bool goal_test(const MCState& s) {
 }
 
 std::vector<MCState> successorsMC(const MCState& s) {
+    using namespace ranges::view;
     std::vector<MCState> successors;
-    auto other_side = Side::EAST;
-    auto sign = -1;
-    if (s.side == Side::WEST) {
-        other_side = Side::WEST;
-        sign = 1;
+    auto sign = (s.side() == Side::WEST) ? -1 : 1;
+    if (s.missionaries() > 1)
+        successors.push_back({s.west_bank_missionaries() + sign * 2, s.west_bank_cannibals(), s.other_side()});
+    if (s.missionaries() > 0)
+        successors.push_back({s.west_bank_missionaries() + sign * 1, s.west_bank_cannibals(), s.other_side()});
+    if (s.cannibals() > 1)
+        successors.push_back({s.west_bank_missionaries(), s.west_bank_cannibals() + sign * 2, s.other_side()});
+    if (s.cannibals() > 0)
+        successors.push_back({s.west_bank_missionaries(), s.west_bank_cannibals() + sign * 1, s.other_side()});
+    if (s.missionaries() > 0 && s.cannibals() > 0)
+        successors.push_back({s.west_bank_missionaries() + sign * 1, s.west_bank_cannibals() + sign * 1, s.other_side()});
+    return successors | remove_if([](const MCState& s) { return !s.is_legal(); });
+}
+
+using Path = std::vector<MCState>;
+
+void print_solution(const Path& p) {
+    auto old_state = p.cbegin();
+    auto current_state = old_state + 1;
+    while (current_state != p.cend()) {
+        int missionaries = old_state->missionaries(), cannibals = old_state->cannibals();
+        if (current_state->side() == Side::WEST) {
+            missionaries -= current_state->east_bank_missionaries();
+            cannibals -= current_state->east_bank_cannibals();
+        } else {
+            missionaries -= current_state->west_bank_missionaries();
+            cannibals -= current_state->west_bank_cannibals();
+        }
+        std::cout
+            << missionaries << " missionaries and " << cannibals << " cannivals"
+            << " moved from the " << old_state->side() << " bank "
+            << " to the " << current_state->side() << " bank.\n";
+        ++old_state, ++current_state;
     }
-    if (s.west_bank_missionaries() > 1)
-        successors.push_back({s.west_bank_missionaries() + sign * 2, s.west_bank_cannibals(), other_side});
-    if (s.west_bank_missionaries() > 0)
-        successors.push_back({s.west_bank_missionaries() + sign * 1, s.west_bank_cannibals(), other_side});
-    if (s.west_bank_cannibals() > 1)
-        successors.push_back({s.west_bank_missionaries(), s.west_bank_cannibals() + sign * 2, other_side});
-    if (s.west_bank_cannibals() > 0)
-        successors.push_back({s.west_bank_missionaries(), s.west_bank_cannibals() + sign * 1, other_side});
-    return successors;
 }
 
 int main() {
